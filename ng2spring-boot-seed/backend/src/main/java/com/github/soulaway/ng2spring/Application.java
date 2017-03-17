4bd33a44458bf1702aacbb3c16e21ad4b43a17ad
soulaway <dgsoloviev@gmail.com>
2017-03-17 04:55:38 +0200

    backend works with API

--- ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/Application.java
+++ ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/Application.java
@@ -1,7 +1,12 @@
 package com.github.soulaway.ng2spring;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +14,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("inspecting Spring Boot's beans:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
---
+++ ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/ControllerImpl.java
@@ -0,0 +1,8 @@
+package com.github.soulaway.ng2spring;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControllerImpl extends com.github.soulaway.myRestApi.api.PetsApiController{
+
+}
