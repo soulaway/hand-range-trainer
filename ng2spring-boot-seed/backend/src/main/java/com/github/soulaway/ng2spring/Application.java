31d1f9ce8d4429ab568b12b438b9618f2133fdb5
soulaway <dgsoloviev@gmail.com>
2017-03-23 11:14:24 +0200

    removed obvious api-jaxrs

--- ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/Application.java
+++ ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/Application.java
@@ -1,7 +1,5 @@
 package com.github.soulaway.ng2spring;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/ControllerImpl.java
+++ ng2spring-boot-seed/backend/src/main/java/com/github/soulaway/ng2spring/ControllerImpl.java
@@ -2,19 +2,22 @@ package com.github.soulaway.ng2spring;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.github.soulaway.myRestApi.model.Range;
+
 import io.swagger.annotations.ApiParam;
 
 
 @Controller()
 @RequestMapping("/api")
 public class ControllerImpl implements com.github.soulaway.myRestApi.api.RangeApi{
+	
 	private List <Range> rangesRep = new ArrayList<Range>();
 	
     public ResponseEntity<Range> addRange(@ApiParam(value = "Range added",required=true ) @PathVariable("id") String id) {
@@ -22,21 +25,29 @@ public class ControllerImpl implements com.github.soulaway.myRestApi.api.RangeAp
         r.setName(id);
         r.setId(new Long(rangesRep.size()));
         rangesRep.add(r);
-        return new ResponseEntity<Range>(HttpStatus.OK).ok(r);
+        return ResponseEntity.ok(r);
     }
     
     public ResponseEntity<Void> deleteRange(@ApiParam(value = "ID of the range to delete",required=true ) @PathVariable("id") Long id) {
-        // do some magic!
-        return new ResponseEntity<Void>(HttpStatus.OK);
+    	Optional<Range> range = rangesRep.stream().filter(r -> r.getId().equals(id)).findFirst();
+    	if (range.isPresent()){
+    		rangesRep.remove(range.get());
+    		return ResponseEntity.ok().build();
+    	} else {
+    		return ResponseEntity.unprocessableEntity().eTag(String.format("Entiity with Id %d was not found", id)).build();
+    	}
     }
 
     public ResponseEntity<Range> findRangeById(@ApiParam(value = "ID of range to fetch",required=true ) @PathVariable("id") Long id) {
-        // do some magic!
-        return new ResponseEntity<Range>(HttpStatus.OK);
+    	Optional<Range> range = rangesRep.stream().filter(r -> r.getId().equals(id)).findFirst();
+    	if (range.isPresent()){
+    		return ResponseEntity.ok(range.get());
+    	} else {
+    		return ResponseEntity.unprocessableEntity().eTag(String.format("Entiity with Id %d was not found", id)).body(null);
+    	}
     }
 
     public ResponseEntity<List<Range>> findRanges() {
-        System.out.println("FRONTEND !!!!");
-        return new ResponseEntity<List<Range>>(HttpStatus.OK).ok(rangesRep);
+        return ResponseEntity.ok(rangesRep);
     }
 }
